d5cf640aac94f25a9253c3a1e4c2cc60446f14f7
sagar-2529 <159178582+sagar-2529@users.noreply.github.com>
2024-08-28 18:43:10 +0530

    Add files via upload

--- lecture40/practice.cpp
+++ lecture40/practice.cpp
@@ -1,19 +1,55 @@
-#include<iostream>
+#include<Iostream>
 #include<vector>
 using namespace std;
 
-int main(){
-int n;
-cin >> n;
-vector<vector<int>> m(n, vector<int>(n));
-   vector<vector<int>> visited(n, vector<int>(n, 0));
-
-for(int i =0 ; i< m.size(); i++){
-    for(int j=0; j<m.size(); j++ ){
-    cout << m[i][j];
-    }cout << endl;
+bool issafe(int x, int y, int n){
+    if (( x >=0 && x<n) && (y>=0 && y<n)){
+        return true;
+    }
+    return false;
 }
 
-cout << endl;
+void solve(int x,int y, string output, int n,vector<string>& ans){
+    if (( x == n-1) &&( y == n-1 )){
+        ans.push_back(output);
+        return ;
+    }
+     
+    
+    //  for right move
+      int newx = x;
+      int newy = y + 1;
+    if (issafe(newx,newy,n)){
+        solve(newx,newy,output+ 'R',n,ans);
+       }
 
+    // for down move
+    newx = x + 1;
+    newy = y; 
+    if (issafe(newx,newy,n)){
+        solve(newx,newy,output+'D',n,ans);
+    }
+
+}
+
+int main(){
+
+    int n;
+    cout << " enter size of matrix : ";
+    cin >> n;
+        string output = "";
+        vector<string> ans;
+        int x = 0;
+        int y = 0;
+        solve(x,y,output,n,ans);
+
+        for(string s: ans){
+            cout << s << endl;
+        }
+
+        
+
+   
+
+    return 0;
 }
